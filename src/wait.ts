4a6049202d14b283fa403e0ae23a307f542cc28c
CrispusDH <qacrispus@gmail.com>
2018-10-08 15:26:51 +0100

    remove redundant code

--- src/wait.ts
+++ src/wait.ts
@@ -1,7 +1,9 @@
+import { Supplier } from './overload-helpers';
+
 export function wait(
-  predicate: Function | Promise<any>,
+  predicate: () => boolean | Supplier<boolean>,
   timeout: number = 0,
-  message: string = undefined,
+  message: string = '',
   pollTimeout: number = 0): Promise<any> {
   if (timeout < 0) {
     throw new Error(`timeout must be a number >= 0: ${timeout}`);
@@ -9,43 +11,11 @@ export function wait(
   if (pollTimeout < 0) {
     throw new Error(`pollTimeout must be a number >= 0: ${pollTimeout}`);
   }
-  
-  if (predicate instanceof Promise) {
-    return new Promise((resolve, reject) => {
-      if (!timeout) {
-        resolve(predicate);
-        return;
-      }
-      
-      let start = Date.now();
-      let timer = setTimeout(() => {
-        timer = null;
-        reject(
-          new Error(
-            (message ? `${message}\n` : '')
-            + 'Timed out waiting for promise to resolve after '
-            + (Date.now() - start) + 'ms'));
-      }, timeout);
-      const clearTimer = () => timer && clearTimeout(timer);
-  
-      predicate.then(
-        (value) => {
-          clearTimer();
-          resolve(value);
-        },
-        (error) => {
-          clearTimer();
-          reject(error);
-        });
-    });
-  }
-  
-  const fn = predicate;
-  
+
   function evaluateCondition() {
     return new Promise((resolve, reject) => {
       try {
-        resolve(fn());
+        resolve(predicate());
       } catch (ex) {
         reject(ex);
       }
@@ -55,14 +25,14 @@ export function wait(
   return new Promise(async (resolve, reject) => {
     const startTime = Date.now();
     const pollCondition = async () => {
-      evaluateCondition().then(function (value) {
+      evaluateCondition().then((value) => {
         const elapsed = Date.now() - startTime;
         if (!!value) {
           resolve(value);
         } else if (timeout && elapsed >= timeout) {
           reject(
             new Error(
-              (message ? `${message}\n` : '')
+              `${message}\n`
               + `Wait timed out after ${elapsed}ms`));
         } else {
           setTimeout(pollCondition, pollTimeout);
